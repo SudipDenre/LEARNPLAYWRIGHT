0b560f1eee7dbc008476b699ed37860eb3ad975b
Sudip Denre <sudipdenre4@gmail.com>
2026-05-12 18:03:00 +0530

    Update chapter_01_Basics/01_Basics.js

--- chapter_01_Basics/01_Basics.js
+++ chapter_01_Basics/01_Basics.js
@@ -1,3 +1,11 @@
 console.log(process.platform);
 console.log(process.arch);
 console.log(process.version);
+
+let x=10;
+console.log(x);
+
+// Hot Code
+/*for(let x=0;x<100000;x++){
+    console.log(x);
+}*/
