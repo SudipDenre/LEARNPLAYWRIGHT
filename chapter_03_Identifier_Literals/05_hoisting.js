2f95fea902669d6afbc2e0ccb577b3729f7d1c3b
Sudip Denre <sudipdenre4@gmail.com>
2026-05-14 09:41:12 +0530

    Update hoisting example in chapter 03

--- chapter_03_Identifier_Literals/05_hoisting.js
+++ chapter_03_Identifier_Literals/05_hoisting.js
@@ -3,5 +3,35 @@ var greeting = "Hello";
 console.log(greeting);
 
 
+//console.log(l); //  TDZ - ReferenceError: Cannot access 'l' before initialization
+let l=30; // TDZ ends
+console.log(l);
+
+
+
+//console.log(c); // TDZ - ReferenceError: Cannot access 'c' before initialization
+const c="Hi";  // TDZ ends
+console.log(c);
+
+
+// Example
+console.log(x); // undefined
+var x = 5;
+function test1() {
+    console.log(x); // undefined
+    var x = 10;
+}
+test1();
+
+// Example
+var a = 1;
+function test2() {
+    console.log(a); // undefined
+    if (true) {
+        var a = 2;
+    }
+    console.log(a); // 2
+}
+test2();
 
 
