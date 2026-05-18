eb3fdb8a69d0e0812b97529b781c6fe2983a516e
Sudip Denre <sudipdenre4@gmail.com>
2026-05-18 14:31:39 +0530

    Update ternary operator examples

--- chapter_03_Identifier_Literals/08_Ternary.js
+++ chapter_03_Identifier_Literals/08_Ternary.js
@@ -6,3 +6,12 @@ let responseTime=850;
 let sla=1000;
 let slaStatus = responseTime<=sla ? "Within SLA" : "SLA Breached";
 console.log(`Response: ${responseTime}ms - ${slaStatus}`);
+
+
+// Nested Ternary
+let statusCode=404;
+let category = statusCode<300 ? "Success" :
+    statusCode<400 ? "Redirect" :
+        statusCode<500 ? "Client Error" : "Server Error";
+
+console.log(`Status Code ${statusCode} - ${category}`);
