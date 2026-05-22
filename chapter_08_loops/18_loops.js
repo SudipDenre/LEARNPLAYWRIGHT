392860eb75f05b54b021b94bfd8221be8ac5be90
Sudip Denre <sudipdenre4@gmail.com>
2026-05-22 12:41:58 +0530

    chore: commit all changes

---
+++ chapter_08_loops/18_loops.js
@@ -0,0 +1,21 @@
+// For, For of, For in , ForEach
+
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+
+// for(let i=0; ; i++){
+//     console.log(i);
+// }  infinite loop - but valid
+
+// let attempt=0;
+// while(attempt<3){
+//     console.log(attempt);
+//     attempt++;
+// }
+
+
+for(let i=0;i<3;i++){
+    if(i===1) continue;
+    console.log(i);
+}
