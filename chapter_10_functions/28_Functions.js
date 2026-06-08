7642d99a72b8198192b0179c7fe999a8ddd33eeb
Sudip Denre <sudipdenre4@gmail.com>
2026-06-08 09:39:35 +0530

    Add chapter 10 functions examples

---
+++ chapter_10_functions/28_Functions.js
@@ -0,0 +1,7 @@
+// Define - Step 1
+function greet(){
+    console.log("Hello! Sudip");
+}
+
+// Call - Step 2
+greet();
---
+++ chapter_10_functions/29_Types_of_Functions.js
@@ -0,0 +1,36 @@
+// without Param, without return
+function greet1(){
+    console.log("Hi Sudip");
+}
+
+greet1();
+
+// with Param, without return
+function greet(name){
+    console.log("Hi ", name);
+}
+
+greet("Abhi");
+
+// without Param, with return
+function greetMe(){
+    return "Hi Kartik";
+}
+
+let c = greetMe();
+console.log(c);
+
+// With Param, with return
+function add(a,b){
+    return a+b;
+}
+
+let sum = add(4,5);
+console.log(sum);
+
+// functions as expression
+const doSomething = function (work){
+    return `${work}ing...`;
+}
+
+console.log(doSomething("Study"));
