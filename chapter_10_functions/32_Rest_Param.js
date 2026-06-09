0e12ba8295921664bd394e6f36c85ff51934c3ab
Sudip Denre <sudipdenre4@gmail.com>
2026-06-10 01:18:47 +0530

    Commit all local changes

---
+++ chapter_10_functions/32_Rest_Param.js
@@ -0,0 +1,7 @@
+function logResult(suiteName, ...results){
+    console.log(suiteName);
+    console.log(results);
+}
+
+logResult("Login Test", 1,2,23,4,5,6);
+logResult("Reg Test", "Hello", "sudip");
---
+++ chapter_10_functions/33_Closure.js
@@ -0,0 +1,13 @@
+function outer(){
+    let message = "hello";
+    console.log("Outer called!");
+
+    function inner(){
+        console.log(message);
+    }
+
+    return inner;
+}
+
+let fn_inner = outer();
+fn_inner();
