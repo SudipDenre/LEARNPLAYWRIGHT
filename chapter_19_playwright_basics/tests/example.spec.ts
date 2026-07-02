995526c301e46ed99660d1af5f3f1fd997f8ac23
Sudip Denre <sudipdenre4@gmail.com>
2026-07-02 09:44:34 +0530

    Add invalid login validation test

--- chapter_19_playwright_basics/tests/example.spec.ts
+++ chapter_19_playwright_basics/tests/example.spec.ts
@@ -4,4 +4,12 @@ test("Verify that the title will be TTACart", async ({page})=>{
   await page.goto("https://app.thetestingacademy.com/playwright/ttacart/");
   await expect(page).toHaveTitle("TTACart - Login");
   await page.waitForTimeout(5000);
+});
+
+test("Verify error message for invalid login credentials", async ({page})=>{
+  await page.goto("https://app.thetestingacademy.com/playwright/ttacart/");
+  await page.fill("#user-name", "invalid_user");
+  await page.fill("#password", "wrong_password");
+  await page.click("#login-button");
+  await expect(page.locator('[data-test="error"]')).toHaveText("Epic sadface: Username and password do not match any user in this service");
 })
